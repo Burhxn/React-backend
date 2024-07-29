5322942d351c449e19eb89922af6116a23d64b14
burhxn <burhxnn@gmail.com>
2024-07-29 06:52:45 +0530

    first

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const connectDb = require("./utils/connectDb");
+const { handleSignUp, handleLogin , handleDelete ,handleEdit , handleGetUser} = require("./controllers/userController");
+const bodyParser = require("body-parser");
+const verifyUser = require("./controllers/userVerification");
+const { config } = require("dotenv");
+// config("/.env")
+
+
+const port = process.env.PORT;
+ 
+// you are creating an instance of express
+
+const server = express(); // inheritance
+
+connectDb();
+
+server.use(cors()); // middle ware
+// server.use(express.json())  // json parsing
+server.use(bodyParser.json())
+
+
+// api routes
+
+server.get("/" , (req,res)=>{res.send("Hello server is working!")})
+server.post("/user/signup" , handleSignUp )
+server.post("/user/login" , handleLogin )
+server.delete("/user/delete/:_id" , handleDelete )
+server.put("/user/edit/:_id" , handleEdit )
+server.get("/user/userDetails/:_id", handleGetUser )
+
+
+server.get("/token/verify/:token", verifyUser )
+
+
+
+
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port} !`);
+});
